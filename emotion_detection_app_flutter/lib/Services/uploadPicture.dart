0924522087a73aa8fa29eebbf8b277a54d3ba48a
Cherinet Hailu-Cherinet-61 <cherinethailu619@gmail.com>
2021-01-01 14:29:09 +0300

    Done with uploading page user interface

--- emotion_detection_app_flutter/lib/Services/uploadPicture.dart
+++ emotion_detection_app_flutter/lib/Services/uploadPicture.dart
@@ -10,7 +10,6 @@ main() {
 class UploadPicture extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    // TODO: implement createState
     return _UploadPicture();
   }
 }
@@ -20,15 +19,66 @@ class _UploadPicture extends State<UploadPicture> {
   AuthService _firebaseAuth = AuthService();
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(
+          actions: <Widget>[
+            FlatButton.icon(
+              onPressed: () async {
+                await _firebaseAuth.userSignOut();
+              },
+              icon: Icon(Icons.person, color: Colors.red),
+              label: Text(
+                "Sign Out",
+                style: TextStyle(color: Colors.red),
+              ),
+            )
+          ],
+        ),
         body: Container(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              FlatButton(onPressed: () async {
-                await _firebaseAuth.userSignOut();
-              }, child: Text('Sign Out'))
+              Container(
+                child: Image.network(
+                   
+                    "https://miro.medium.com/max/500/0*-ouKIOsDCzVCTjK-.png",width:400,height:300,
+                    fit: BoxFit.fill,),
+                    
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  RaisedButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(23.0),
+                        side: BorderSide(color: Colors.cyanAccent)),
+                    onPressed: () {},
+                    color: Colors.blue[50],
+                    textColor: Colors.white,
+                    child: Text("Camera",
+                        style: TextStyle(fontSize: 14, color: Colors.blueGrey)),
+                  ),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  RaisedButton(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(23.0),
+                        side: BorderSide(color: Colors.cyanAccent)),
+                    onPressed: () {},
+                    color: Colors.blue[50],
+                    textColor: Colors.white,
+                    child: Text("Gallery",
+                        style: TextStyle(fontSize: 14, color: Colors.blueGrey)),
+                  ),
+                ],
+              )
             ],
           ),
         ),
